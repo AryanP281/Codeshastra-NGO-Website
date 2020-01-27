38e4db43a0bb79561ea83d32eb0aad3f956ae8d2
Aryan Pathare <33375954+AryanP281@users.noreply.github.com>
2020-01-27 16:19:45 +0530

    Update run.py
    Added some missing functionality

--- python/run.py
+++ python/run.py
@@ -24,25 +24,20 @@ Column("dons_rcv", Integer),
 Column("hlpd", Integer)
 )
 
-ndy = needy.select()
-conn = engine.connect()
-res = conn.execute(ndy)
-
-needy_profiles_count = list(res).__len__()
-
-s = ngos.select()
-res = conn.execute(s)
-registered_ngos =  list(res).__len__()
-
 @app.route("/",methods=["GET","POST"])
 def home() :
         return render_template("index.html")
 
+@app.route("/success", methods=["GET","POST"])
+def success_page() :
+    if request.method == "POST":
+        return redirect(f"{url_for('home')}")
+    else :
+        return render_template("success.html")
+
 @app.route("/createneedyprofile", methods=["GET", "POST"])
 def create_needy_profile() :
     if request.method == "POST" :
-        global name, age, dl, needy_profiles_count
-        needy_profiles_count += 1
         profile = [ request.form["name"],
         request.form["age"],
         request.form["gender"],
@@ -50,41 +45,30 @@ def create_needy_profile() :
         request.form["phydis"],
         request.form["loc"] ]
         add_to_needy_database(profile)
-        return render_template("success.html")
+        return redirect(f"{url_for('success_page')}")
     else :
          return render_template("needy_profile.html")
 
 @app.route("/registerngo", methods=["GET", "POST"])
 def register_ngo() :
     if request.method == "POST" :
-        global registered_ngos
-        registered_ngos += 1
         data = [ request.form["name"],
         request.form["loc"],
         request.form["don"],
         request.form["nop"]
         ]
-
-        ins_op = ngos.insert().values(id=registered_ngos,name=data[0],loc=data[1],dons_rcv=data[2],hlpd=data[3])
-        conn = engine.connect()
-        res = conn.execute(ins_op) 
-
+        add_to_ngo_database(data)
         return redirect("/")
     else :
         return render_template("RegisterNgo.html")
 
-@app.route("/locatengo", methods=["GET", "POST"])
-def locate_ngo() :
-    if request.method == "POST" :
-        return redirect("/")
-    else :
-        return render_template("search.html")
-
 @app.route("/ngolist")
 def ngo_list() :
-    s = ngos.select()
     conn = engine.connect()
-    res = conn.execute(s)
+
+    command = f"""SELECT * FROM ngos"""
+
+    res = conn.execute(command)
 
     lst = []
     for e in res :
@@ -98,13 +82,30 @@ def search_by_loc() :
         url = f"/ngoprofile/" + str(request.form["ngo"])
         return redirect(url)
     else :
-        return render_template("search.html")
+        conn = engine.connect()
+
+        locs = []
+        ngos_lst = []
+        
+        ngos_select = conn.execute("SELECT * FROM ngos")
+
+        for ngo in ngos_select :
+            if(ngo[2] in locs) :
+                ngos_lst[locs.index(ngo[2])].append(ngo[1])
+            else :
+                locs.append(ngo[2])
+                ngos_lst.append([])
+                ngos_lst[-1].append(ngo[1])
+
+        return render_template("search.html", locs=locs, ngos_lst=ngos_lst)
 
 @app.route("/needylist")
 def needy_list() :
-    s = ndy.select()
     conn = engine.connect()
-    res = conn.execute(s)
+
+    command = f"""SELECT * FROM n_people"""
+
+    res = conn.execute(command)
 
     lst = []
     for e in res :
@@ -114,17 +115,21 @@ def needy_list() :
 
 @app.route("/needyprofile/<name>")
 def needy_profile(name) :
-    s = ndy.select().where(ndy.c.name == name)
     conn = engine.connect()
-    prof = list(conn.execute(s))  
+
+    command = f"""SELECT * FROM n_people WHERE name = '{name}'"""
+
+    prof = list(conn.execute(command))  
 
     return render_template("needy_profile_display.html", name=prof[0][1], age=prof[0][2], sex=prof[0][3], bg=prof[0][4], disability=prof[0][5], loc=prof[0][6])
 
 @app.route("/ngoprofile/<name>", methods=["GET", "POST"])
 def ngo_profile(name):
-    s = ngos.select().where(ngos.c.name == name)
     conn = engine.connect()
-    prof = list(conn.execute(s))
+
+    command = f"""SELECT * FROM ngos WHERE name = '{name}'"""
+
+    prof = list(conn.execute(command))
     
     if request.method == "GET" :
         return render_template("ngo_profile.html", name = prof[0][1], loc=prof[0][2], dons=prof[0][3], hlpd=prof[0][4])
@@ -134,9 +139,11 @@ def ngo_profile(name):
 
 @app.route("/donate/<name>", methods=["GET","POST"])
 def donate(name) :
-    s = ngos.select().where(ngos.c.name == name)
     conn = engine.connect()
-    prof = list(conn.execute(s))
+
+    command = f"""SELECT * FROM ngos WHERE name = '{name}'"""
+
+    prof = list(conn.execute(command))
     
     if request.method == "POST" :
         upd = ngos.update().where(ngos.c.name == name).values(dons_rcv=int(prof[0][3]) + int(request.form["mtrans"]))
@@ -145,16 +152,22 @@ def donate(name) :
     else :
         return render_template("payment_details.html", ngo=name)
 
-@app.route("/debug")
-def dbg() :
-    return f"{needy_profiles_count}"
-
 
 def add_to_needy_database(data) :
-    ins_op = needy.insert().values(id=needy_profiles_count,name=data[0],age=data[1],sex=data[2],bg=data[3],disability=data[4],loc=data[5])
     conn = engine.connect()
-    res = conn.execute(ins_op)  
+    command = f"""INSERT INTO n_people(name, age, sex, bg, disability, loc) 
+    VALUES ('{data[0]}', '{data[1]}', '{data[2]}', '{data[3]}', '{data[4]}', '{data[5]}')"""
+    
+    res = conn.execute(command) 
+
+def add_to_ngo_database(data) :
+    conn = engine.connect()
+
+    command = f"""INSERT INTO ngos(name, loc, dons_rcv, hlpd) 
+    VALUES ('{data[0]}', '{data[1]}', '{data[2]}', '{data[3]}')"""
+
+    res = conn.execute(command) 
 
 
 if(__name__ == "__main__") :
-    app.run(debug=True)
+    app.run(debug=False)
